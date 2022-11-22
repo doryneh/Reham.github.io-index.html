b39222335d8b5ed46e31c1b8069ae1a9e6945049
Dory Nehme <doryy.nehme@gmail.com>
2022-11-23 00:08:59 +0200

    first commit

---
+++ js/SideNavScroll.js
@@ -0,0 +1,19 @@
+var nav = $('.SideNav');
+$(window).on('scroll', function () {
+    $(document).ready(function () {
+        if ($(window).scrollTop() >= nav.offset().top + nav.outerHeight() - window.innerHeight) {
+            $('.Scrollable-Part').css("position", "absolute");
+            $('.Scrollable-Part').css("bottom", "0%")
+            $('.Scrollable-Part').css("width", "23%");
+
+        }
+        else {
+
+            $('.Scrollable-Part').css("position", "fixed");
+            $('.Scrollable-Part').css("bottom", "unset")
+            $('.Scrollable-Part').css("width", "23%");
+
+
+        }
+    });
+});
