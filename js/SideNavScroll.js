2edd44096454b68a5f3b02db58279b154a7aab73
Dory Nehme <doryy.nehme@gmail.com>
2023-09-22 20:30:47 +0300

    fixing

--- js/SideNavScroll.js
+++ js/SideNavScroll.js
@@ -4,15 +4,14 @@ $(window).on('scroll', function () {
         if ($(window).scrollTop() >= nav.offset().top + nav.outerHeight() - window.innerHeight) {
             $('.Scrollable-Part').css("position", "absolute");
             $('.Scrollable-Part').css("bottom", "0%")
-            $('.Scrollable-Part').css("width", "23%");
+            $('.Scrollable-Part').css("width", "100%")
 
         }
         else {
 
             $('.Scrollable-Part').css("position", "fixed");
             $('.Scrollable-Part').css("bottom", "unset")
-            $('.Scrollable-Part').css("width", "23%");
-
+            $('.Scrollable-Part').css("width", "25%")
 
         }
     });
